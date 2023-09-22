ef6b53ec771aa049c4430199b97aaa9b0fcba5ef
Noor-Alrai <ameinnoor91@gmail.com>
2023-09-22 17:53:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Noor Alrai";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = (
+  "Welcome to Flatbook, " +
+  currentUser +
+  "!"
+).toUpperCase();
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
